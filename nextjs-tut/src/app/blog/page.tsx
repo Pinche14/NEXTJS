287d386dc53e9bb4b5f8eff5a30001775d6c8cc5
Pinche14 <shubham70254@gmail.com>
2025-02-10 22:43:16 +0530

    first commit

---
+++ nextjs-tut/src/app/blog/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Metadata } from 'next'
+
+
+export const metadata: Metadata = {
+  title: "Blog"
+}
+
+export default function Blog(){
+  return (
+    <h1>My blog</h1>
+  )
+}
