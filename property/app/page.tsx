c6178b0848cc8240278453d9c93ff71ddd985385
unknown <Pinche14>
2025-04-21 18:03:20 +0530

    Property

---
+++ property/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from 'antd'
+import React from 'react'
+
+const page = () => {
+  return (
+    <Button type="primary">
+      Hello
+    </Button>
+  )
+}
+
+export default page
---
+++ property/config/themeConfig.ts
@@ -0,0 +1,9 @@
+import type {ThemeConfig} from "antd/es/config-provider/context";
+
+const theme: ThemeConfig = {
+    token: {
+        colorPrimary: "#35C991",
+    },
+}
+
+export default theme;
